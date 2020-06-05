4f27fdf2e9f13016c1328257ed3711f7e3c7da8c
Amanda Shih <pandafulmanda@gmail.com>
2020-06-05 09:00:05 -0500

    parcels toggle

--- js/main.js
+++ js/main.js
@@ -172,3 +172,41 @@ map.on('load', function () {
     labelLayerId
   );
 });
+
+var toggleableLayerIds = ['parcels'];
+
+function toggleLayer(layerName) {
+  var visibility = map.getLayoutProperty(layerName, 'visibility');
+
+  // toggle layer visibility by changing the layout object's visibility property
+  if (visibility === 'visible') {
+    map.setLayoutProperty(layerName, 'visibility', 'none');
+    this.className = '';
+  } else {
+    this.className = 'active';
+    map.setLayoutProperty(layerName, 'visibility', 'visible');
+  }
+}
+
+
+// set up the corresponding toggle button for each layer
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+  var id = toggleableLayerIds[i];
+
+  var link = document.createElement('a');
+  link.href = '#';
+  link.className = 'active';
+  link.textContent = id;
+
+  link.onclick = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    toggleLayer('parcel-fills');
+    toggleLayer('parcel-no-vals');
+    toggleLayer('parcel-lines');
+  };
+
+  var layers = document.getElementById('menu');
+  layers.appendChild(link);
+}
